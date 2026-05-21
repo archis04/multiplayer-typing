06f597075e45cde7858a3ef11f01965c63973e7c
Archis <archis.borwankar2020@gmail.com>
2026-05-21 21:42:10 +0530

    typestring solved

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,8 @@ import { WebSocketServer } from "ws";
 import { generateRandomString } from "./lib/generatetext";
 import { RoomManager } from "./RoomManager";
 
-const wss = new WebSocketServer({port:8081})
+const PORT = parseInt(process.env.PORT || '8081', 10);
+const wss = new WebSocketServer({port:PORT})
 const roomManager = new RoomManager();
 wss.on("connection",async(ws)=>{
     console.log("User joined")
